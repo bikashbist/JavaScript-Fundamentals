85e08d8b9dfa3efd0733135dba791067a9554a76
bikashbist <bikas.bist@yahoo.com>
2024-10-15 19:41:49 +0545

    Basic Operators

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
+
+// subtraction
 const ageRam = 2020 -1990;
+// addition
+const ageHari = ageRam + 30;
+// multiplication
+let A = 30;
+let B = 10;
+
+let multi = A * B;
+
 console.log(ageRam);
+console.log(ageHari);
+console.log(multi);
