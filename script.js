f78821f242c6d77b746530fae77da37a8aede2dd
bikashbist <bikas.bist@yahoo.com>
2024-10-16 12:10:00 +0545

    template literals

---
+++ script.js
@@ -0,0 +1,10 @@
+const firstName = "bikash";
+const job = "Teacher";
+const BirthYear = 1995;
+const year = 2021;
+
+const bikash = "i'm " + firstName + ', a '  + job + ' ' + (year-BirthYear)+  'years old';
+console.log(bikash);
+
+const bikashNew = `i'm ${firstName}, a ${year-BirthYear} years old ${job}!`;
+console.log(bikashNew)
