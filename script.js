5d2d04209577c23f1664f99f717d69ded4a2ead3
bikashbist <bikas.bist@yahoo.com>
2024-10-16 18:51:38 +0545

    calculate century using if else statemt

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ else{
     const ageLeft = 18 - age;
     console.log(`you are too young. wait another  ${ageLeft} year` )
 }
+
+const birthYear = 1991;
+let century;
+if(birthYear <= 2000){
+     century = 20;
+} else{
+     century = 21;
+}
+console.log(century);
